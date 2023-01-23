1e1e8ac7cf5157e9945b82502543e2753e8f3ddd
ren dunning <laurendunningcs@gmail.com>
2023-01-23 14:44:34 -0500

    Add time.sleep library

--- DeclarationofIndependance.py
+++ DeclarationofIndependance.py
@@ -6,5 +6,9 @@
 Our Welcome screen will start out program letting drivers know that the InfoTechCenter is loading
 """
 
+#Import Libraries Here
+import time
+timeToSleep = 2
 print("\n\nWelcome - InfoTechCenter 4.0")
+time.sleep(timeToSleep)
 print("InfoTechCenter is loading")

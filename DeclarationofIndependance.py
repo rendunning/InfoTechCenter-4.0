71689893cd33ce33b1b780b42e2775126659e300
ren dunning <laurendunningcs@gmail.com>
2023-01-23 14:42:09 -0500

    Add print statements

---
+++ DeclarationofIndependance.py
@@ -0,0 +1,10 @@
+# Programmer: Lauren Dunning
+# Date: 1.20.2023
+# Program: InfoTechCenter Upgrades
+
+"""
+Our Welcome screen will start out program letting drivers know that the InfoTechCenter is loading
+"""
+
+print("\n\nWelcome - InfoTechCenter 4.0")
+print("InfoTechCenter is loading")

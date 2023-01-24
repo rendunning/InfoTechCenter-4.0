efc8f7c11a65305d22039bd8d022ca1f3791e457
ren dunning <laurendunningcs@gmail.com>
2023-01-24 14:09:36 -0500

    Add colors and loading dots

--- DeclarationofIndependance.py
+++ DeclarationofIndependance.py
@@ -8,7 +8,23 @@ Our Welcome screen will start out program letting drivers know that the InfoTech
 
 #Import Libraries Here
 import time
-timeToSleep = 2
-print("\n\nWelcome - InfoTechCenter 4.0")
-time.sleep(timeToSleep)
-print("InfoTechCenter is loading")
+import sys
+
+print('\n\033[1;34;48m Welcome to InfoTechCenter')
+
+x = 0
+a = 0
+
+time.sleep(2)
+print('')
+
+while x != 20:
+    x += 1
+    b = ("\033[1;33;40m InfoTechCenter is loading" + "." * a)
+    a = a + 1
+    sys.stdout.write('\r'+b)
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print('\033[1;32;40m Done!')

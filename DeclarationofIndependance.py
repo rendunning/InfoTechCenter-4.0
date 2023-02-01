cdb2e93ddae9cdf58b91612f11214eaead96a402
ren dunning <laurendunningcs@gmail.com>
2023-02-01 14:09:22 -0500

    Added GasLevelAlert Function - WIP
    if == Empty elif == low

--- DeclarationofIndependance.py
+++ DeclarationofIndependance.py
@@ -27,10 +27,9 @@ def gasLevelGauge():
     currentGasLevel = random.choice(gasLevelList)
     return currentGasLevel
 
-print(gasLevelGauge())
 
 # Variable calling gasLevelGauge function to store its value
-gasLevelIndicator = gaslevelGauge()
+gasLevelIndicator = gasLevelGauge()
  
 # List of Gas Stations Function
  
@@ -38,3 +37,20 @@ def listOfGasStations():
     gasStations = ["shell", "Costco", "Sam's Club", "Buc-ee's", "7/11", "Speedway", "Meijer"]
     gasStationNearby = random.choice(gasStations)
     return gasStationNearby
+
+# Determine Gas Level & Closest gas station
+def gasLevelAlert():
+    milesToGasStationLow = round(random.uniform(1, 25), 2)
+    milesToGasStationQuarterTank = round(random.uniform(1, 50), 2)
+    if gasLevelIndicator == "Empty":
+        print("***WARNING YOUR TANK IS EMPTY***")
+        sleep(1)
+        print("Calling Emergency Contact")
+    elif gasLevelIndicator == "Low":
+        print("*****Warning*****")
+        sleep(1)
+        print("Your gas tank is extremely low; checking Google Maps for the closest gas station.")
+        sleep(1)
+        print("The closest gas station is", listOfGasStations(),"which is",milesToGasStationLow, "miles away.")
+
+gasLevelAlert()

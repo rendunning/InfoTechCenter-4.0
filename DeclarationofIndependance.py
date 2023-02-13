c6449ba120626f33f82a275138a611a1329b59aa
ren dunning <laurendunningcs@gmail.com>
2023-02-13 13:59:57 -0500

    spelling corrections

--- DeclarationofIndependance.py
+++ DeclarationofIndependance.py
@@ -17,20 +17,20 @@ weatherAlert = weather()
 # VRS() to respond to the weather condition
 def vehicleResponseSystem():
 	if weatherAlert == "snow":
-		print("\nNWS has changed your alarm by 15 minutes because of the weather forecast of",weatherAlert)
+		print("\nNWS has changed your alarm by 15 minutes because of the weather forecast of",weatherAlert,".")
 		print("VRS has been engaged only allowing your vehicle to go 45 MPH")
 	elif weatherAlert == "blizzard":
-		print("\nNWS has changed your alarm by 30 minutes because of the weather forecast of",weatherAlert)
+		print("\nNWS has changed your alarm by 30 minutes because of the weather forecast of",weatherAlert,".")
 		print("VRS has been engaged only allowing your vehicle to go 35 MPH")
+	elif weatherAlert == "icy":
+		print("\nNWS has changed your alarm by 60 minutes because of the weather forecast of",weatherAlert,"conditions, please drive carefully.")
+		print("VRS has been engaged only allowing your vehicle to go 25 MPH")
 	elif weatherAlert == "rain":
-		print("\nNWS is calling for",weatherAlert,",please drive extra carefully.")
+		print("\nNWS is calling for",weatherAlert,",please drive carefully.")
 	elif weatherAlert == "foggy":
 		print("\nNWS is calling for",weatherAlert,"conditions, please drive carefully.")
 	elif weatherAlert == "windy":
 		print("\nNWS is calling for",weatherAlert,"conditions, please drive carefully.")
-	elif weatherAlert == "icy":
-		print("\nNWS has changed your alarm by 60 minutes because of the weather forecast of",weatherAlert,"conditions, please drive carefully.")
-		print("VRS has been engaged only allowing your vehicle to go 25 MPH")
 	else:
 		print("\nNWS is calling for",weatherAlert,",drive safely and have a wonderful day.")
 

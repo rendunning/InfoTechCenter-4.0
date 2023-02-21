ce4d4f591fd2bb85f09aae1cbae948d30ef591ea
Lucas Campbell <lucass.ccampbell@gmail.com>
2023-02-21 14:18:53 -0500

    Initial commit + import libraries

--- DeclarationofIndependance.py
+++ DeclarationofIndependance.py
@@ -2,6 +2,8 @@
 # Date: 2/21/2023
 # Program: Navigation
 
-"""
 
-"""
+# This function will take the input of your house and it will tell you how far away it is from your current location.
+
+# Import libraries here
+import random

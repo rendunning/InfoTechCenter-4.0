b55e523c7d2cd2198a524ae684d44b84fdef2104
Lauren Dunning <laurendunningcs@gmail.com>
2023-03-01 12:19:49 -0500

    Merge branch 'WelcomeScreen' into DevTesting

--- DeclarationofIndependance.py
+++ DeclarationofIndependance.py
@@ -20,7 +20,29 @@ Create a Function to determine our gas level and closest gas station
 import random
 from time import sleep
 
+#Import Libraries Here
+import time
+import sys
 
+print('\n\033[1;34;48m Welcome to InfoTechCenter')
+
+x = 0
+a = 0
+
+time.sleep(2)
+print('')
+
+while x != 20:
+    x += 1
+    b = ("\033[1;33;35m InfoTechCenter is loading" + "." * a)
+    a = a + 1
+    sys.stdout.write('\r'+b)
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print('\033[1;32;35m Done!')
+        
 # Gas Level Function
 def gasLevelGauge():
     gasLevelList = ["Empty", "Low", 'Quarter Tank', "Half Tank", "Three Quarter Tank", "Full Tank"]
